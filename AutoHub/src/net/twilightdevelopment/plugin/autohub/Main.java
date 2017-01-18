b9a8df3f9719055ab83646fe4f58282e23cdb500
BuildTools <unconfigured@null.spigotmc.org>
2017-01-17 19:32:12 -0700

    Test

---
+++ AutoHub/src/net/twilightdevelopment/plugin/autohub/Main.java
@@ -0,0 +1,23 @@
+package net.twilightdevelopment.plugin.autohub;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	public void onEnable() {
+		
+		Bukkit.getServer().getPluginManager().registerEvents(new Join(this), this);
+		
+		Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+		
+		getCommand("hub").setExecutor(new HubCommand(this));
+		getCommand("sethub").setExecutor(new HubCommand(this));
+		saveConfig();
+		saveDefaultConfig();
+		}
+	
+	public void onDisable() {
+		saveDefaultConfig();
+	}
+}
