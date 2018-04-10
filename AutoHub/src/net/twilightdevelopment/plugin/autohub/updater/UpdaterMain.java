64fd2767172d984865d592c95b71ae70107436a3
Caelum100 <caelum12321@gmail.com>
2018-04-09 19:50:06 -0600

    Changed Spigot resource update URL

--- AutoHub/src/net/twilightdevelopment/plugin/autohub/updater/UpdaterMain.java
+++ AutoHub/src/net/twilightdevelopment/plugin/autohub/updater/UpdaterMain.java
@@ -3,14 +3,19 @@ package net.twilightdevelopment.plugin.autohub.updater;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.*;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class UpdaterMain implements Runnable {
 	private JavaPlugin plugin;
 	private String currentVersion;
 
-	private static final String API_URL = "https://api.spigotmc.org/legacy/general.php?resource=";
+	private static final String API_URL = "https://api.spigotmc.org/legacy/update.php?resource=";
 	private static final String PREFIX = "[AutoHub] ";
 	private static final String RESOURCE_ID = "34966";
 
@@ -22,11 +27,11 @@ public class UpdaterMain implements Runnable {
 	public void run() {
 		boolean newVersionFound = false;
 		while (!newVersionFound) {
-			HttpURLConnection con = null;
+			HttpsURLConnection con = null;
 			BufferedReader reader = null;
 			try {
 				URL url = new URL(API_URL + RESOURCE_ID);
-				con = (HttpURLConnection) url.openConnection();
+				con = (HttpsURLConnection) url.openConnection();
 				con.setRequestMethod("GET");
 				con.setReadTimeout(20 * 1000);
 
@@ -36,17 +41,23 @@ public class UpdaterMain implements Runnable {
 
 				if (latestVersion != null && !currentVersion.equals(latestVersion)) {
 					Bukkit.getConsoleSender().sendMessage(
-							PREFIX + "A new version is available! Please download it from the plugin resource page; "
+							PREFIX + ChatColor.AQUA + "A new version is available! Please download it from the plugin resource page; "
 									+ "you will receive no support regarding old versions of the plugin.");
 					newVersionFound = true;
-					
-					// Check every hour
-					Thread.sleep(1000 * 60 * 60); 
 				}
 			} catch (Exception e) {
 				if (con != null)
 					con.disconnect();
+				
+				// DEBUG
+				e.printStackTrace();
 			}
+			// Check every hour
+			try {
+				Thread.sleep(1000 * 60 * 60);
+			} catch (InterruptedException e) {
+				
+			} 
 		}
 	}
 }
